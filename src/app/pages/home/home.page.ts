1fea45ff3b74b4f2c448fd33cd1e46ff49944b49
Munblanc <robe.martinez@duocuc.cl>
2024-09-10 20:48:24 -0300

    funcion de boton arma tu menu y back button desde receta del dia para volver a home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  irARecetaDelDia() {
+    this.navCtrl.navigateForward('/home/recetadeldia');  
   }
-
 }
