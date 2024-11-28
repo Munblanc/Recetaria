88a3eac557e28ff3b1bf83fcbe74fefc5732c85a
LilGhostly <ma.navarreteg@duocuc.cl>
2024-11-28 04:13:10 -0300

    passwords

--- src/app/services/openai.service.ts
+++ src/app/services/openai.service.ts
@@ -8,17 +8,17 @@ import { initializeApp, getApps } from 'firebase/app';
 })
 export class OpenaiService {
   private openai: OpenAI;
-  private apiKey: string = 'sk-SOn5Xnu1UY-tx4mBehAmE-_fgPoFP-k0N5UJWFm3qjT3BlbkFJnmmlFpTNyd1_AuiIjxwzMwFg82e7K3HAyK6ZAZoCAA';
+  private apiKey: string = '';
 
   constructor() {
     const firebaseConfig = {
-      apiKey: 'AIzaSyBbrSHxu7kbEsx5S3a_7-u8t04PUa3RdrQ',
-      authDomain: 'recetaria-2024.firebaseapp.com',
-      databaseURL: 'https://recetaria-2024-default-rtdb.firebaseio.com',
-      projectId: 'recetaria-2024',
-      storageBucket: 'recetaria-2024.appspot.com',
-      messagingSenderId: '439253985358',
-      appId: '1:439253985358:web:7bbe51e89835eed04fbb1f',
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: '',
+      appId: '',
     };
 
     if (getApps().length === 0) {
